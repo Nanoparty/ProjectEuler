7545ddbe65ba1269836406416538e9d683ef9b1f
Necropuppy <nfooteaz@gmail.com>
2021-03-27 20:05:09 -0400

    12. complete

---
+++ 12. Highly divisible triangular number/main.py
@@ -0,0 +1,21 @@
+from time import time
+t = time()
+import math
+
+def divisors(n):
+    number_of_factors = 0
+    for i in range(1, int(math.ceil(math.sqrt(n)))):
+        if n % i == 0:
+            number_of_factors +=2
+        else:
+            continue
+    return number_of_factors
+
+x=1
+for y in range(2,1000000):
+    x += y
+    if divisors(x) >= 500:
+        print(x)
+        break
+tt = time()-t
+print(tt)
